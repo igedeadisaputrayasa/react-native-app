638bf4b6ab5dfd105f52069f3ac613f86d813762
I Gede Adi Saputra Yasa <igedeadisaputra.yasa@gmail.com>
2017-11-30 16:17:48 +0800

    keyboard avoiding

--- HelloWorld/App/Container/App.js
+++ HelloWorld/App/Container/App.js
@@ -17,10 +17,18 @@ import LoginForm from './LoginForm';
 export default class App extends Component {
   render() {
     return (
-      <KeyboardAvoidingView behavior="padding">
+      <KeyboardAvoidingView behavior="padding" style={styleApp.container}>
         <Text>Inventory System</Text>
         <LoginForm />
       </KeyboardAvoidingView>
     );
   }
 }
+
+
+const styleApp = StyleSheet.create({
+  container : {
+      flex : 1
+  }
+});
+
