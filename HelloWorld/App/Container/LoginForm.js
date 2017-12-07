7a2759deb944e8d3d47da86d427851b0d7764c6a
I Gede Adi Saputra Yasa <igedeadisaputra.yasa@mitrais.com>
2017-12-07 08:04:03 -0800

    changes

--- HelloWorld/App/Container/LoginForm.js
+++ HelloWorld/App/Container/LoginForm.js
@@ -10,6 +10,45 @@ import {
 import IconVector from 'react-native-vector-icons/Ionicons';
 
 export default class LoginForm extends Component {
+    constructor(props){
+		super(props)
+		this.state={
+			userName:'',
+			userPassword:''
+		}
+	}
+    login = () =>{
+        const {userName,userPassword} = this.state;
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+        if(userName==""){
+			alert("Please enter username");		    
+		}else if(userPassword==""){
+            alert("Please enter password");
+        }else{            
+            fetch('http://localhost:8000/oauth/v2/token',{
+                method:'get',
+                header:{
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body:JSON.stringify({
+                    username: userName,
+                    password: userPassword,
+                    grant_type: 'password',
+                    client_id: '1_kbi45ho80i888o4wc08s8k8gs04woggwsg4s84swk04ocgoc8',
+                    client_secret: '54b6sr58v6ccwkscso0kgwgos80wk84oco4occ40scgk8k0wo8'
+                })                
+            })
+            .then((response) => response.json())
+                .then((responseJson)=>{
+                    alert("Successfully Login");
+                })
+                .catch((error)=>{
+                    console.error(error);
+                });
+            }
+    }
+
     render() {
         return (
             <View style={styleLoginForm.container}>
@@ -25,14 +64,16 @@ export default class LoginForm extends Component {
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
-                    placeholder='username or email' />
+                    placeholder='username or email' 
+                    onChangeText={userName => this.setState({userName})} />
                 <TextInput 
                     style={styleLoginForm.input} 
                     secureTextEntry
                     returnKeyType="go"
                     placeholder='password' 
-                    ref={(input) => this.passwordInput = input } />
-                <TouchableOpacity style={styleLoginForm.buttonLogin}>
+                    ref={(input) => this.passwordInput = input } 
+                    onChangeText={userPassword => this.setState({userPassword})} />
+                <TouchableOpacity style={styleLoginForm.buttonLogin} onPress={this.login}>
                     <Text style={styleLoginForm.buttonTextLogin}>Sign in</Text>
                 </TouchableOpacity>
             </View>
